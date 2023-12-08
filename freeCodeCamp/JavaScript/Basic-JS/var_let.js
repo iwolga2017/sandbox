e276ae050693cec4ab4de08d493e8b7ad7cbbb0e
iwolga2017 <iwolga2017@mail.ru>
2023-12-08 15:58:33 +0300

    Create var_let.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/var_let.js
@@ -0,0 +1,6 @@
+// So unlike var, when you use let, a variable with the same name can only be declared once.
+
+// Update the code so it only uses the let keyword.
+
+let catName = "Oliver";
+let catSound = "Meow!";
