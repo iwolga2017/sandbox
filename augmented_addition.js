829e95e4de947aa99c3ec9f4b06fdca4388e4176
iwolga2017 <iwolga2017@mail.ru>
2023-12-09 15:18:37 +0300

    added augmented_addition.js

---
+++ augmented_addition.js
@@ -0,0 +1,12 @@
+// Convert the assignments for a, b, and c to use the += operator.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+console.log(a);
+console.log(b);
+console.log(c);
