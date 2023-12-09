85f7cc5dfb94ec16a9ac79dc82f901b1eec4dfb5
iwolga2017 <iwolga2017@mail.ru>
2023-12-09 15:25:15 +0300

    created augmented_subtraction.js

---
+++ augmented_subtraction.js
@@ -0,0 +1,13 @@
+// Convert the assignments for a, b, and c to use the -= operator.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a);
+console.log(b);
+console.log(c);
