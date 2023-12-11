3f9845a24cfe9bc41891e62423a605d5ba5d8993
iwolga2017 <iwolga2017@mail.ru>
2023-12-11 22:26:47 +0300

    created augmented_division.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/augmented_division.js
@@ -0,0 +1,13 @@
+// Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a);
+console.log(b);
+console.log(c);
