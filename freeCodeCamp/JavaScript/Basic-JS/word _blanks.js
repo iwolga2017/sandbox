4440f95621d78fb41becc99737cf1d6a58baa33f
iwolga2017 <iwolga2017@mail.ru>
2023-12-12 15:18:11 +0300

    Create word _blanks.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/word _blanks.js
@@ -0,0 +1,13 @@
+// You will need to use the string concatenation operator + to build a new string,
+// using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. 
+// You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+// You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. 
+// The result should be a complete sentence.
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb; 
+console.log(wordBlank);
