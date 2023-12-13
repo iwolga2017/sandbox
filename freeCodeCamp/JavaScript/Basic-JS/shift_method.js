fa5e4a3a5caccd970c3b33672a84ae1e07e28456
iwolga2017 <iwolga2017@mail.ru>
2023-12-13 14:35:57 +0300

    Create shift_method.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/shift_method.js
@@ -0,0 +1,8 @@
+// Use the .shift() function to remove the first item from myArray
+// and assign the "shifted off" value to a new variable, removedFromMyArray.
+
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+
+console.log(removedFromMyArray);
+console.log(myArray);
