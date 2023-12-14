b88ad111052981f5f5ca9468789dd7838daf5cd8
iwolga2017 <iwolga2017@mail.ru>
2023-12-14 14:04:03 +0300

    created return_in_function.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/return_in_function.js
@@ -0,0 +1,7 @@
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(n) {
+    return n *= 5;
+}
+
+console.log(timesFive(4));
