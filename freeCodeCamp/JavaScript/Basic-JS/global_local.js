d9db8e8662544461305a5bf0ac68469592f8ebff
iwolga2017 <iwolga2017@mail.ru>
2023-12-14 14:58:36 +0300

    created global_local.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/global_local.js
@@ -0,0 +1,12 @@
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  const outerWear = "sweater";
+  return outerWear;
+}
+
+console.log(myOutfit());
+
+
