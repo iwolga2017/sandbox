78a536e9d1265d8217dcc4b87c48920f44e78781
iwolga2017 <iwolga2017@mail.ru>
2023-12-15 22:30:29 +0300

    created if_statements.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/if_statements.js
@@ -0,0 +1,16 @@
+// Create an if statement inside the function to return Yes,
+//  that was true if the parameter wasThatTrue is true 
+//  and return No, that was false otherwise.
+
+
+function trueOrFalse(wasThatTrue) {
+
+    if (wasThatTrue) {
+        return "Yes, that was true";
+    }
+
+    return "No, that was false";
+}
+
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
