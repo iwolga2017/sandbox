a69add3f509b31d491dd7e7c29676c5137d761ae
iwolga2017 <iwolga2017@mail.ru>
2023-12-18 21:27:03 +0300

    created else_if_statements.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/else_if_statements.js
@@ -0,0 +1,18 @@
+// Convert the logic to use else if statements.
+
+function  testElseIf(val) {
+
+    if (val > 10) {
+        return "Greater than 10";
+    }
+    else if (val < 5) {
+        return "Smaller than 5";
+    }
+    else {
+        return "Between 5 and 10";
+    }
+}
+
+console.log(testElseIf(7));
+console.log(testElseIf(11));
+console.log(testElseIf(-6));
