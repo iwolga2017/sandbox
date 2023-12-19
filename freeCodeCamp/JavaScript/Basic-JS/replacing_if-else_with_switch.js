7877fd0eb915b2e3193aa32e53b3278f37aa3fa1
iwolga2017 <iwolga2017@mail.ru>
2023-12-19 13:23:13 +0300

    Create replacing_if-else_with_switch.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/replacing_if-else_with_switch.js
@@ -0,0 +1,27 @@
+// Change the chained if/else if statements into a switch statement.
+
+function chainToSwitch(val) {
+  let answer = "";
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  return answer;
+}
+
+console.log(chainToSwitch(99));
+console.log(chainToSwitch("bob"));
