44e4a2e5c458a4063249179ac7969c7295986529
iwolga2017 <iwolga2017@mail.ru>
2023-12-19 13:27:39 +0300

    Create f_return_boolean.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/f_return_boolean.js
@@ -0,0 +1,9 @@
+// Fix the function isLess to remove the if/else statements. 
+
+function isLess(a, b) {
+  return a < b;
+}
+
+console.log(isLess(10, 15));
+console.log(isLess(30, 8));
+console.log(isLess(205, 11));
