c17e22a1e687c681969f5174276f0ba40418f9e3
iwolga2017 <iwolga2017@mail.ru>
2023-12-20 21:04:18 +0300

    created delete_prop_from_obj.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/delete_prop_from_obj.js
@@ -0,0 +1,16 @@
+// Delete the tails property from myDog.
+//  You may use either dot or bracket notation
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+
+
+  delete myDog.tails;
+  delete myDog["bark"];
+
+  console.log(myDog);
