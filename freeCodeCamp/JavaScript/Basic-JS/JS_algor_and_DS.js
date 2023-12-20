f6c3bd5f3c42f958bf3ea2cfbc101253355b3b95
iwolga2017 <iwolga2017@mail.ru>
2023-12-20 21:19:31 +0300

    created JS_algor_and_DS.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/JS_algor_and_DS.js
@@ -0,0 +1,21 @@
+// Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string 
+// to the result variable
+
+function phoneticLookup(val) {
+
+    let result = "";
+
+    const lookup = {
+
+        alpha: "Adams",
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank"
+    };
+        result = lookup[val];
+        return result;
+}
+
+console.log(phoneticLookup("charlie"));
