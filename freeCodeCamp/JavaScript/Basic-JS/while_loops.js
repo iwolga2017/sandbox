e1e56f58e82c65a4766938a33f5519af6d6bc16a
iwolga2017 <iwolga2017@mail.ru>
2023-12-22 23:49:22 +0300

    created while_loops.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/while_loops.js
@@ -0,0 +1,23 @@
+const ourArray = [];
+let i = 0;
+
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
+
+//  In the code example above, the while loop will 
+// execute 5 times and append the numbers 0 through 4 to ourArray.
+
+// Add the numbers 5 through 0 (inclusive) in 
+// descending order to myArray using a while loop.
+
+const myArray = [];
+ i = 5;
+
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+
+console.log(myArray);
