fd740cb93fd7cab12a356b205407ee89e6573bf6
iwolga2017 <iwolga2017@mail.ru>
2023-12-23 22:24:17 +0300

    created iterate_odd_numbers.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/iterate_odd_numbers.js
@@ -0,0 +1,10 @@
+// Push the odd numbers from 1 
+// through 9 to myArray using a for loop.
+
+const myArray = [];
+
+for (let i = 1; i < 10; i += 2) {
+    myArray.push(i);
+}
+
+console.log(myArray);
