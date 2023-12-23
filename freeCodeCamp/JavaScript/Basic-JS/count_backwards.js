f91db0ed9eaec8f4d99531e7eec8d6132593bf2f
iwolga2017 <iwolga2017@mail.ru>
2023-12-23 22:32:23 +0300

    created count_backwards.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/count_backwards.js
@@ -0,0 +1,11 @@
+// Push the odd numbers from 9 through 1 to myArray using
+//  a for loop
+
+
+const myArray = [];
+
+for (let i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+
+console.log(myArray);
