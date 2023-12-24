09fd5ff9feddd75571ebbd6ad0ad3f06db6b5ff2
iwolga2017 <iwolga2017@mail.ru>
2023-12-25 01:22:07 +0300

    created parseInt_func.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/parseInt_func.js
@@ -0,0 +1,11 @@
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, 
+// and returns it.
+
+function convertToInteger(str) {
+
+    return parseInt(str);
+}
+
+console.log(convertToInteger("654"));
+console.log(convertToInteger("46"));
+console.log(convertToInteger("955745"));
