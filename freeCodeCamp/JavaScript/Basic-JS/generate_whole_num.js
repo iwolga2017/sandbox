23a4a858abb6c441a943313598da56838866dd2b
iwolga2017 <iwolga2017@mail.ru>
2023-12-25 00:49:51 +0300

    created generate_whole_num.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/generate_whole_num.js
@@ -0,0 +1,11 @@
+// Use this technique to generate and return a random whole 
+// number in the range from 0 to 9
+
+function randomWholeNum() {
+
+  return  Math.floor( Math.random() * 10 );
+}
+
+    console.log(randomWholeNum());
+    console.log(randomWholeNum());
+    console.log(randomWholeNum());          
