7f9df71a676a9c5e53c52e9bc8947611ee9403b3
iwolga2017 <iwolga2017@mail.ru>
2023-12-25 00:43:01 +0300

    created Math_random.js

---
+++ freeCodeCamp/JavaScript/Basic-JS/Math_random.js
@@ -0,0 +1,10 @@
+// Change randomFraction to return a random
+//  number instead of returning 0
+
+function randomFraction() {
+
+    return Math.random();
+}
+
+console.log(randomFraction());
+console.log(randomFraction());
