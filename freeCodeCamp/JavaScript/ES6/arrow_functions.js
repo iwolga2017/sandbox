d23ff9fc43d134a9a05217d8fc408549d0d2c2c1
iwolga2017 <iwolga2017@mail.ru>
2023-12-28 23:53:08 +0300

    created arrow_functions.js

---
+++ freeCodeCamp/JavaScript/ES6/arrow_functions.js
@@ -0,0 +1,7 @@
+// Rewrite the function assigned to the variable magic 
+// which returns a new Date() to use arrow function syntax. Also,
+// make sure nothing is defined using the keyword var.
+
+const magic = () => { return new Date(); };
+
+console.log(magic());
