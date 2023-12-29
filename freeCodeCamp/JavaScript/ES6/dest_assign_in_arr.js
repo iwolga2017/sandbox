e7d71b1835b0683d2f36da298861634a6fc53cbf
iwolga2017 <iwolga2017@mail.ru>
2023-12-30 00:16:26 +0300

    created dest_assign_in_arr.js

---
+++ freeCodeCamp/JavaScript/ES6/dest_assign_in_arr.js
@@ -0,0 +1,23 @@
+// Use destructuring assignment to swap the values of a and b 
+// so that a receives the value stored in b, and b receives the value 
+// stored in a.
+
+let a = 8, b = 6;
+
+[a, b] = [b, a];
+
+console.log(a);
+console.log(b);
+
+let x = 7;
+    y = 10;
+    z = 92;
+    k = 57;
+
+
+[x, y, z, k] = [y, x, k, z];
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(k);
