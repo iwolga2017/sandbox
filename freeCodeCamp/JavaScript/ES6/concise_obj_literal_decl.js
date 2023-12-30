51991398cb4f7d6194fedfa1057b634a08e8d412
iwolga2017 <iwolga2017@mail.ru>
2023-12-30 23:36:40 +0300

    Create concise_obj_literal_decl.js

---
+++ freeCodeCamp/JavaScript/ES6/concise_obj_literal_decl.js
@@ -0,0 +1,6 @@
+// Use object property shorthand with object literals to create and return an 
+// object with name, age and gender properties.
+
+const createPerson = (name, age, gender) => ({ name, age, gender });
+
+console.log(createPerson(("Zodiac Hasbro", 56, "male"));
