12098e5c55a7bc7b8384538bfbd7d0cd3df53433
iwolga2017 <iwolga2017@mail.ru>
2024-01-02 22:40:49 +0300

    created import_a_default_export.js

---
+++ freeCodeCamp/JavaScript/ES6/import_a_default_export.js
@@ -0,0 +1,9 @@
+// In the following code, import the default export
+//  from the math_functions.js file, found in the same 
+//  directory as this file. Give the import the name subtract
+
+
+import subtract from "./export_default.js";
+
+// subtract is NOT surrounded by curly braces {};
+// it's a variable for whatever the default export;
