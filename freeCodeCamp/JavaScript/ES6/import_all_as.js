dd38c83111e649019df1659285b6e80b9ea7d9bd
iwolga2017 <iwolga2017@mail.ru>
2024-01-02 22:18:18 +0300

    created import_all_as.js

---
+++ freeCodeCamp/JavaScript/ES6/import_all_as.js
@@ -0,0 +1,7 @@
+// The code in this file requires the contents of the file:
+//  string_functions.js, that is in the same directory as the
+//  everything from the file into an object called 
+//  stringFunctions.
+//  current file. Use the import * as syntax to import 
+
+import * as stringFunctions from "./export_to_share_a_code.js";
