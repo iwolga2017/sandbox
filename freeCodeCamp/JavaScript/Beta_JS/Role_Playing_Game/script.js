90c70ba97ee1cfb83ac73f7847762a6fe28004ff
iwolga2017 <iwolga2017@mail.ru>
2024-01-06 23:00:42 +0300

    filled in the content goTown()

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -27,6 +27,13 @@ button3.onclick = fightDragon();
 
 function goTown() {
 
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 };
 
 function goStore() {
