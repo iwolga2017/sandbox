78b2a67233e750e85663e9d2796f852abf53d816
iwolga2017 <iwolga2017@mail.ru>
2024-01-07 01:16:34 +0300

    created match_literal_str.js

---
+++ freeCodeCamp/JavaScript/Regular_Expressions/match_literal_str.js
@@ -0,0 +1,8 @@
+// Complete the regex waldoRegex to find "Waldo" in the string
+//  waldoIsHiding with a literal match
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/;
+let result = waldoRegex.test(waldoIsHiding);
+
+console.log(result);
