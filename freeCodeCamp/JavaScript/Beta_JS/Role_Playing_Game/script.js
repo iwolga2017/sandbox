07ecd4a86df96d2898de6e8caf394f07f6b7f536
iwolga2017 <iwolga2017@mail.ru>
2024-01-06 23:34:53 +0300

    declared update f-n and locations const with 1 obj

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -19,11 +19,24 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    }
+];
+
 // initialize buttons
 
 button1.onclick = goStore();
 button2.onclick = goCave();
 button3.onclick = fightDragon();
+
+function update(location) {
+
+};
 
 function goTown() {
 
