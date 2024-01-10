4796896e863dca9a9a5da6e2f6638b5a0f44f2c3
iwolga2017 <iwolga2017@mail.ru>
2024-01-11 00:35:06 +0300

    added if statement in buyWeapon()

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -100,7 +100,15 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon ++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+    }
 }
 
 function fightSlime() {
