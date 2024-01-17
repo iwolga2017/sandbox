994f22fe1fe4017e6903541d3e132c0120cd25b7
iwolga2017 <iwolga2017@mail.ru>
2024-01-17 23:45:10 +0300

    added monsters [] and moved fightDragon()

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -18,23 +18,17 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
 const weapons = [
-    {
-        name: "stick",
-        power: 5
-    },
-    {
-        name: "dagger",
-        power: 30
-    },
-    {
-        name: "claw hammer",
-        power: 50
-    },
-    {
-        name: "sword",
-        power: 100
-    }
-];
+    { name: "stick",  power: 5 },
+    { name: "dagger", power: 30 },
+    { name: "claw hammer", power: 50 },
+    { name: "sword", power: 100 }
+  ];
+
+  const monsters = [
+    { name: "slime", level: 2, health: 15 },
+    { name: "fanged beast", level: 8, health: 60 },
+    { name: "dragon", level: 20, health: 300 }
+  ];
 
 const locations = [
   {
@@ -82,10 +76,6 @@ function goStore() {
 
 function goCave() {
   update(locations[2]);
-}
-
-function fightDragon() {
-  console.log("Fighting dragon.");
 }
 
 function buyHealth() {
@@ -140,4 +130,8 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
