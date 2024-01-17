896570ab3ca8d0face50f217f50b708a0bea2fa1
iwolga2017 <iwolga2017@mail.ru>
2024-01-18 00:17:24 +0300

    added obj "fight" in locations

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -48,6 +48,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -151,5 +157,5 @@ function attack() {
 }
 
 function dodge() {
-  
+
 }
