13205e54107881932e184d5d04227e1dd88aec19
iwolga2017 <iwolga2017@mail.ru>
2024-01-21 00:26:02 +0300

    added display to none for monster's stat box

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -59,7 +59,7 @@ const locations = [
     name: "kill monster",
     "button text": ["Go to town square", "Go to town square", "Go to town square"],
     "button functions": [goTown, goTown, goTown],
-    text: "The monster screams Arg! as it dies. You gain experience points and find gold."
+    text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
   }
 ];
 
@@ -69,6 +69,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
