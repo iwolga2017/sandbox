fa5ac07282b097b83a3fe94441c3f6f592dcf14a
iwolga2017 <iwolga2017@mail.ru>
2024-01-25 14:53:09 +0300

    added for loop in pick()

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -254,7 +254,10 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));  
   }
-  text.innerText = "You picked " + guess + ". Here are the random numbers:";
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
 }
 
 function pickTwo() {
