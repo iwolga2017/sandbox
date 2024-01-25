28f8508a86c9dfe71484b631e9ebe4ff8572096f
iwolga2017 <iwolga2017@mail.ru>
2024-01-25 14:36:57 +0300

    added while loop in pick()

--- freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Role_Playing_Game/script.js
@@ -251,6 +251,9 @@ function easterEgg() {
 
 function pick(guess) {
   const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));  
+  }
 }
 
 function pickTwo() {
