64fa9059efd4b2a2dc27d8cb10dbebd8ba2f13c3
iwolga2017 <iwolga2017@mail.ru>
2024-01-29 20:11:45 +0300

    created collection_of_data_in_array.js

---
+++ freeCodeCamp/JavaScript/Basic_Data_Structures/collection_of_data_in_array.js
@@ -0,0 +1,9 @@
+// We have defined a variable called yourArray. 
+// Complete the statement by assigning an array of at least 5 
+// elements in length to the yourArray variable. 
+// Your array should contain at least one string, 
+// one number, and one boolean.
+
+let yourArray = [1, "one", true, null, undefined, {1: "name", 2: "surname"}];
+
+console.log(yourArray);
