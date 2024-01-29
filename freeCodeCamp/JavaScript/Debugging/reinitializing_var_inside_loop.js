2a862267fe8f15b5e832c6e75ce6ab7acd869153
iwolga2017 <iwolga2017@mail.ru>
2024-01-29 15:01:59 +0300

    Create reinitializing_var_inside_loop.js

---
+++ freeCodeCamp/JavaScript/Debugging/reinitializing_var_inside_loop.js
@@ -0,0 +1,22 @@
+// The following function is supposed to create a two-dimensional array with m rows and n columns of zeroes.
+// Unfortunately, it's not producing the expected output because the row variable 
+// isn't being reinitialized (set back to an empty array) in the outer loop. 
+// Fix the code so it returns a correct 3x2 array of zeroes, which looks like [[0, 0], [0, 0], [0, 0]]
+
+function zeroArray(m, n) {
+  let newArray = [];
+  let row = [];
+  for (let i = 0; i < m; i++) {
+    // Adds the m-th row into newArray
+    newArray[i] = row;
+    for (let j = 0; j < n; j++) {
+      // Pushes n zeroes into the current row to create the columns
+      row[j] = 0;
+    }
+   
+  }
+  return newArray;
+}
+
+let matrix = zeroArray(3, 2);
+console.log(matrix);
