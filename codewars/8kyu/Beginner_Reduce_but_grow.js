c299d8f65fd5caf5f05ec4593a3433536726705d
iwolga2017 <iwolga2017@mail.ru>
2024-01-30 23:14:38 +0300

    Beginner_Reduce_but_grow.js

---
+++ codewars/8kyu/Beginner_Reduce_but_grow.js
@@ -0,0 +1,15 @@
+// Given a non-empty array of integers, return the result 
+// of multiplying the values together in order. 
+
+// Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+  
+    return x.reduce((mult, current) => mult * current, 1);
+  
+  }
+
+  console.log(grow([1, 2, 3]));
+  console.log(grow([4, 1, 1, 1, 4]));
