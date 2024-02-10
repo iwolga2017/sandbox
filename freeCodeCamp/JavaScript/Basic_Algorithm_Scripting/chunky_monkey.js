3edacabf34e0de18b559d3d7fb13d671ef32bf8c
iwolga2017 <iwolga2017@mail.ru>
2024-02-10 18:30:21 +0300

    Create chunky_monkey.js

---
+++ freeCodeCamp/JavaScript/Basic_Algorithm_Scripting/chunky_monkey.js
@@ -0,0 +1,13 @@
+// Write a function that splits an array (first argument) into groups the 
+// length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+  let result =[];
+  for (let i = 0; i < arr.length; i+= size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+
