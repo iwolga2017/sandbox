86b662446cd49cf031c9c5d09f1b458c65ce1924
iwolga2017 <iwolga2017@mail.ru>
2024-02-11 11:02:26 +0300

    Create constructor_function.js

---
+++ freeCodeCamp/JavaScript/OOP/constructor_function.js
@@ -0,0 +1,8 @@
+// Create a constructor, Dog, with properties name, color, 
+// and numLegs that are set to a string, a string, and a number, respectively.
+
+function Dog() {
+  this.name = "Woof";
+  this.color = "brown";
+  this.numLegs = 4;
+}
