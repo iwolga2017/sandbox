c213c0d5bd73c1bd8ade2c05faba97e72af4eeee
iwolga2017 <iwolga2017@mail.ru>
2024-02-26 11:19:17 +0300

    Create Remove Elements with slice Method.js

---
+++ freeCodeCamp/JavaScript/Functional_Programming/Remove Elements with slice Method.js
@@ -0,0 +1,10 @@
+// Rewrite the function nonMutatingSplice by using slice instead of splice. 
+// It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
+
+// Do not mutate the original array provided to the function.v
+
+function nonMutatingSplice(cities) {
+   
+  return cities.slice(0, 3);
+}
+console.log(nonMutatingSplice(["Chicago", "Delhi", "Islamabad", "London", "Berlin"]));
