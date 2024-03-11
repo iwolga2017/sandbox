23c8a3e853f3f5363318eff72f7f5efe516a08a1
iwolga2017 <iwolga2017@mail.ru>
2024-03-11 14:35:45 +0300

    created Drop.it

---
+++ freeCodeCamp/JavaScript/Intermediate_Algorithm_Scripting/Drop it.js
@@ -0,0 +1,15 @@
+// Given the array arr, iterate through and remove each element starting 
+// from the first element (the 0 index) until the function func returns true 
+// when the iterated element is passed through it.
+// Then return the rest of the array once the condition is satisfied, 
+// otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+    while (arr.length > 0 && !func(arr[0])) {
+       arr.shift();
+     }
+     return arr;
+   }
+
+   console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+   console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
