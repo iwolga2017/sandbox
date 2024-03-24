c29d04853ae56d1e6719998059603eadfeec3380
iwolga2017 <iwolga2017@mail.ru>
2024-03-24 12:19:12 +0300

    changed for loop with regex in cleanInputString()

--- freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
@@ -8,12 +8,6 @@ let isError = false;
 
 function cleanInputString(str) {
 
-    let strArray = str.split('');
-    let cleanStrArray = [];
-
-    for (let i = 0; i < strArray.length; i++) {
-       if (!["+", "-", " "].includes(strArray[i])) {
-        cleanStrArray.push(strArray[i]);
-       } 
-    }
+    let regex = /[+-\s]/g;
+    return str.replace(regex, "");
  }
