7bd2cd01648ca139fe27dce00a96aa84613ff480
iwolga2017 <iwolga2017@mail.ru>
2024-03-27 13:47:35 +0300

    added label and input in HTMLString

--- freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
@@ -22,5 +22,6 @@ function addEntry() {
 
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]'.length);
-    const HTMLString = ``;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">`;
 }
