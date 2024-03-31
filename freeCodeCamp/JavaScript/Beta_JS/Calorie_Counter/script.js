f8413348f9ae98f5bfad5628b036c79ac4d5746b
iwolga2017 <iwolga2017@mail.ru>
2024-03-31 22:28:29 +0300

    created lunchCalories var in calculateCalories()

--- freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
@@ -38,6 +38,7 @@ function calculateCalories(e) {
     const snacksNumberInputs = document.querySelectorAll("#snacks input[type=number]");
     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type=number]");
     const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+    const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
 }
 
 function getCaloriesFromInputs(list) {
