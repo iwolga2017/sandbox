0ce98df4063c2f4400bd84fe74b12fd942211509
iwolga2017 <iwolga2017@mail.ru>
2024-04-01 14:02:52 +0300

    add if stat in calculateCalories()

--- freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
@@ -43,6 +43,10 @@ function calculateCalories(e) {
     const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
     const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+    if (isError) {
+        return;
+    }
 }
 
 function getCaloriesFromInputs(list) {
