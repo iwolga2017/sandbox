e9073912679af18c1660a37f2cc44c2ff3cc7da7
iwolga2017 <iwolga2017@mail.ru>
2024-04-02 11:27:05 +0300

    created p elem in output.innerHTML

--- freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Calorie_Counter/script.js
@@ -53,7 +53,9 @@ function calculateCalories(e) {
     output.innerHTML = `
     <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
     <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
     `;
+    
 }
 
 function getCaloriesFromInputs(list) {
