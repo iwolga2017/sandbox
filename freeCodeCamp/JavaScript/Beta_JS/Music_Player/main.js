c022fc320fd36b9ec0a4d9f679f5af40c38afd40
iwolga2017 <iwolga2017@mail.ru>
2024-04-08 14:02:08 +0300

    added 2 spans for artist and duration in button

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -91,6 +91,8 @@ const songsHTML = array.map(songs => {
   <li id="song-${song.id}" class="playlist-song"></li>
   <button class="playlist-song-info">
   <span class="playlist-song-title"> ${song.title} </span>
+  <span class="playlist-song-artist">${song.artist}</span>
+  <span class="playlist-song-duration">${song.duration}</span>
   </button>
   `
 })
