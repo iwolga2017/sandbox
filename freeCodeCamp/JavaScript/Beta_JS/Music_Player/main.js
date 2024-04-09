086fc5fc1ed692b151e5cb837d98f4244926e9c0
iwolga2017 <iwolga2017@mail.ru>
2024-04-09 14:42:08 +0300

    filled the function in sort()

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -99,8 +99,21 @@ const songsHTML = array.map(songs => {
 }).join("");
 playlistSongs.innerHTML = songsHTML
 };
-renderSongs(userData?.songs)
 
 const sortSongs = () => {
-  userData?.songs.sort()
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  })
 }
+
+renderSongs(userData?.songs)
+
+
