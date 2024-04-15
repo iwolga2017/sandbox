8af25b264d3848842268c8020efb1e53557a9de6
iwolga2017 <iwolga2017@mail.ru>
2024-04-15 16:29:09 +0300

    add Event Listener to pauseButton onclick

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -102,7 +102,6 @@ const playSong = (id) => {
 }
 
 const pauseSong = () => {
-
   userData.songCurrentTime = audio.currentTime
   playButton.classList.remove('playing')
   audio.pause()
@@ -133,6 +132,8 @@ playButton.addEventListener('click', () => {
     playSong(userData?.currentSong.id)
   }
 })
+
+pauseButton.addEventListener('click', pauseSong)
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
