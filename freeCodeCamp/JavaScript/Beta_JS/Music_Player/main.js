2e938fc49e106454a2b7bf571e66ff940704298f
iwolga2017 <iwolga2017@mail.ru>
2024-04-17 15:09:23 +0300

    added if else stat in playPreviousSong

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -118,7 +118,11 @@ const playNextSong = () => {
 }
 
 const playPreviousSong = () => {
-  
+  if (userData?.currentSong === null) {
+    return
+  } else {
+    const currentSongIndex = getCurrentSongIndex()
+  }
 }
 
 const renderSongs = (array) => {
