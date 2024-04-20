e7b32df877a5f191c29c6171bc31b4ff45d8eedb
iwolga2017 <iwolga2017@mail.ru>
2024-04-20 14:07:07 +0300

    remove "aria-current" attr by songEl

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -131,7 +131,8 @@ const playPreviousSong = () => {
   const playlistSongElements = document.querySelectorAll('.playlist-song')
   const songToHighlight = document.getElementById(`song-${userData?.currentSong?.id}`)
    playlistSongElements.forEach(songEl => {
-  
+    
+    songEl.removeAttribute("aria-current")
 })
 }
 
