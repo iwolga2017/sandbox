7f58d1c197dd7e61a99dc702f48c03c4ec70c521
iwolga2017 <iwolga2017@mail.ru>
2024-04-22 21:03:29 +0300

    created currentTitle and  currentArtist  consts

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -132,6 +132,8 @@ const setPlayerDisplay = () => {
 
   const playingSong = document.getElementById('player-song-title')
   const songArtist = document.getElementById('player-song-artist')
+  const currentTitle = userData?.currentSong?.title
+  const currentArtist = userData?.currentSong?.artist
 }
 
  const highlightCurrentSong = () => {
