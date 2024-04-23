30c5fc47846ac72b66ef3b2f9df99d0c4d69c679
iwolga2017 <iwolga2017@mail.ru>
2024-04-23 12:35:58 +0300

    created setPlayButtonAccessibleText func

--- freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
+++ freeCodeCamp/JavaScript/Beta_JS/Music_Player/main.js
@@ -170,6 +170,10 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML
 };
 
+const setPlayButtonAccessibleText = () => {
+  
+}
+
 const getCurrentSongIndex = () => {
   
   return userData?.songs.indexOf(userData?.currentSong)
