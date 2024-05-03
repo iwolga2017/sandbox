a882c7725a264cd37dd43f2955d2a8700d33b899
iwolga2017 <iwolga2017@mail.ru>
2024-05-03 13:27:28 +0300

    created count var

--- freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
@@ -1,7 +1,2 @@
 let character = 'Hello';
-console.log(character);
-character = "World";
-console.log(character);
-let secondCharacter;
-secondCharacter = character;
-console.log(secondCharacter);
+let count = 8;
