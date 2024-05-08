d065d8d57440a3fc3b834a735889c1078582c6d6
iwolga2017 <iwolga2017@mail.ru>
2024-05-08 15:15:35 +0300

    Create Sentence to words.js

---
+++ codewars/7kyu/Sentence to words.js
@@ -0,0 +1,11 @@
+// Write function splitSentence which will create a list of strings from a string.
+
+// Example:
+
+// "hello world" -> ["hello", "world"]
+
+function splitSentence(s) {
+  return s.split(" ");
+}
+
+console.log(splitSentence("hello world"));
