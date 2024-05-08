2961e5a619c763df85ec0dc14274a2dcf35d8941
iwolga2017 <iwolga2017@mail.ru>
2024-05-08 15:18:02 +0300

    Create Words to sentence.js

---
+++ codewars/7kyu/Words to sentence.js
@@ -0,0 +1,12 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+// Example:
+
+// ["hello", "world"] -> "hello world"
+
+function wordsToSentence(words) {
+  return words.join(" ");
+}
+
+console.log(wordsToSentence(["hello", "world"]));
+
