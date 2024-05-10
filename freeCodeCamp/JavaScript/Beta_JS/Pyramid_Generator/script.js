60fedef43d2b68e23829d877fd8e54da6734670c
iwolga2017 <iwolga2017@mail.ru>
2024-05-10 16:06:40 +0300

    using multi-line comment

--- freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
@@ -7,9 +7,10 @@ function padRow(rowNumber, rowCount) {
 }
 
 // change the code to a different kind of loop
-for (let i = 1; i <= count; i++) {
+/*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count)); 
- }
+}
+ */
 
   let result = "";
 
