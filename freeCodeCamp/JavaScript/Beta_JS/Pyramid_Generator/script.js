7efeaffa05db142b4f7a069bdc6312d37173fd49
iwolga2017 <iwolga2017@mail.ru>
2024-05-13 18:10:08 +0300

    check if inverted is true in the loop

--- freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Pyramid_Generator/script.js
@@ -10,6 +10,7 @@ function padRow(rowNumber, rowCount) {
 // change the code to a different kind of loop
 
 for (let i = 1; i <= count; i++) {
+  if (inverted)
   rows.unshift(padRow(i, count)); 
 }
  
