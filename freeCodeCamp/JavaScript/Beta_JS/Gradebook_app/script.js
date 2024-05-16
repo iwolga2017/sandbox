c2994b2f9f674a5eebf5cdb83fab2b9f0f537d68
iwolga2017 <iwolga2017@mail.ru>
2024-05-16 14:13:26 +0300

    created func hasPlayerWonTheRound

--- freeCodeCamp/JavaScript/Beta_JS/Gradebook_app/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Gradebook_app/script.js
@@ -2,5 +2,14 @@ function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     let index = Math.floor(Math.random() * options.length);
     return options[index];
-  }
-  console.log(getRandomComputerResult());
+}
+console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+    return ( // if the cond true return true or false
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Paper") ||
+        (player === "Paper" && computer === "Rock")
+    );
+
+}
