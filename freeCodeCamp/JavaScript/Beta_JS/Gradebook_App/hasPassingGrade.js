6782a843617c0e49d05180b1d7f2ca16458df856
iwolga2017 <iwolga2017@mail.ru>
2024-05-23 12:19:14 +0300

    check if a student has a passing grade

---
+++ freeCodeCamp/JavaScript/Beta_JS/Gradebook_App/hasPassingGrade.js
@@ -0,0 +1,25 @@
+function hasPassingGrade(score) {
+  
+    function getGrade(score) {
+      if (score === 100) {
+       return "A++";
+     } else if (score >= 90 && score <= 99) {
+       return "A";
+     } else if (score >= 80 && score <= 89) {
+       return "B";
+     } else if (score >= 70 && score <= 79) {
+       return "C";
+     } else if (score >= 60 && score <= 69) {
+       return "D";
+     } else if (score >= 0 && score <= 59) {
+       return "F";
+     }
+    }
+    return getGrade(score) === "F" ? false : true;
+  
+  }
+  
+  
+  console.log(hasPassingGrade(100));
+  console.log(hasPassingGrade(53));
+  console.log(hasPassingGrade(87));
