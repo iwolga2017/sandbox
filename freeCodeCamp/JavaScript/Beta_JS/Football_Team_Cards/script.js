eeed22879ba1949a070e86b29f32a24183d2ec3a
iwolga2017 <iwolga2017@mail.ru>
2024-05-31 19:56:04 +0300

    rewrote vars with destructuring assignment

--- freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
@@ -173,5 +173,8 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
-const sport = myFavoriteFootballTeam.sport;
-const team = myFavoriteFootballTeam.team;
+
+// const sport = myFavoriteFootballTeam.sport;
+// const team = myFavoriteFootballTeam.team;
+
+const {sport, team} = myFavoriteFootballTeam;
