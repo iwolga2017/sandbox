557f96e598a2b22e890e6a7ff873549cf6dfb09f
iwolga2017 <iwolga2017@mail.ru>
2024-06-04 15:26:42 +0300

    Create Thinking & Testing: A and B.js

---
+++ codewars/7kyu/Thinking & Testing: A and B.js
@@ -0,0 +1,11 @@
+// No Story
+// No Description
+// Only by Thinking and Testing
+// Look at result of testcase, guess the code!
+// 01: A and B?
+
+function testit(a,b){
+  return a | b;
+}
+
+console.log(testit(10, 20))
