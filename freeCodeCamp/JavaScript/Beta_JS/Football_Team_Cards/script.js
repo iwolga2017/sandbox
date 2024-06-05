a7381d98aab8a07583a6eaf8ee5a8bc5dc0a8f05
iwolga2017 <iwolga2017@mail.ru>
2024-06-05 20:18:52 +0300

    set e as a param

--- freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
@@ -199,4 +199,4 @@ const setPlayerCards = (arr = players)  => {
     }).join("");
 }
 
-playersDropdownList.addEventListener("change", () => {})
+playersDropdownList.addEventListener("change", (e) => {})
