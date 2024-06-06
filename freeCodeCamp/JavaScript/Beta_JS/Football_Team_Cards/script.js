e12ae32a85508c470105d119bb93ab95692779de
iwolga2017 <iwolga2017@mail.ru>
2024-06-06 15:27:20 +0300

    add case "defender"

--- freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
+++ freeCodeCamp/JavaScript/Beta_JS/Football_Team_Cards/script.js
@@ -211,6 +211,9 @@ playersDropdownList.addEventListener("change", (e) => {
     case "midfielder":
       setPlayerCards(players.filter(player => player.position === "midfielder"));
       break;
+    case "defender":
+      setPlayerCards(players.filter((player) => player.position === "defender"));
+      break;
   }
 
 })
